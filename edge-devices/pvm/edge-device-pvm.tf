bc6e6d12c50029d095bb90001f7b2c76a7a37a75
Ondrej Krajicek <ondrej@krajicek.co>
2022-10-03 18:53:47 -0500

    CACertificates copied from iotedge github repo based on https://learn.microsoft.com/en-us/azure/iot-edge/how-to-create-test-certificates\?view\=iotedge-1.4\&tabs\=linux.

---
+++ edge-devices/pvm/edge-device-pvm.tf
@@ -0,0 +1,12 @@
+{
+  "type": "parallels-iso",
+  "guest_os_type": "ubuntu",
+  "iso_url": "http://releases.ubuntu.com/12.04/ubuntu-12.04.3-server-amd64.iso",
+  "iso_checksum": "2cbe868812a871242cdcdd8f2fd6feb9",
+  "parallels_tools_flavor": "lin",
+  "ssh_username": "packer",
+  "ssh_password": "packer",
+  "ssh_timeout": "30s",
+  "shutdown_command": "echo 'packer' | sudo -S shutdown -P now"
+}
+
